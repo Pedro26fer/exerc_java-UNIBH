00b7643ee19023d38061db77ad0594cba8a09392
Pedrohf <p2698fernandes@gmail.com>
2025-04-25 18:40:34 -0300

    build: first commit

---
+++ Exerc01.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Exerc01{
+
+    public static int somaElementos(int[] vetor){
+        int soma = 0;
+        for(int i = 0; i < vetor.length; i++){
+            soma = soma + vetor[i];
+        }
+        return soma;
+    }
+
+
+    public static void main([]Strings args){
+        int [] array = new int[10];
+        Scanner scanner = new Scanner(System.in);
+        aux_buildArray = 0;
+
+        while(aux_buildArray)
+    }
+}
